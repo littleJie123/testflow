354e0578f168a52b6c87e9775fbec19c5f918770
littlejie123 <ytrgmj@gmail.com>
2026-07-07 12:01:42 +0800

    运营平台

--- client/js/util.js
+++ client/js/util.js
@@ -49,4 +49,30 @@ class Util {
       return '<span class="' + cls + '">' + match + '</span>';
     });
   }
+
+  static showToast(message, type = 'success', duration = 2000) {
+    let container = document.getElementById('toastContainer');
+    if (!container) {
+      container = document.createElement('div');
+      container.id = 'toastContainer';
+      container.className = 'toast-container';
+      document.body.appendChild(container);
+    }
+    const toast = document.createElement('div');
+    toast.className = 'toast ' + type;
+    toast.textContent = message;
+    container.appendChild(toast);
+    requestAnimationFrame(() => {
+      toast.classList.add('show');
+    });
+    setTimeout(() => {
+      toast.classList.remove('show');
+      setTimeout(() => {
+        toast.remove();
+        if (container.childElementCount === 0) {
+          container.remove();
+        }
+      }, 300);
+    }, duration);
+  }
 }
